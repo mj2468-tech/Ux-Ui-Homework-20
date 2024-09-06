f3c0597f4f93a8ea83dee71ff3fbf14a76ecfc5e
mj2468-tech <maggiecjones@hotmail.com>
2024-09-05 22:21:26 -0600

    Add files via upload

---
+++ Interactions.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+    <><html><link rel="stylesheet" type="text/css" href="interactions.css"></link>
+        <body>
+        <div class="hero-bkg-animated">
+  <h1>Hero Headline</h1>
+</div>
+<div class="jumbotron">
+<h1 class="display-4">My Portfolio</h1>
+<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+<hr class="my-4">
+<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+
+<!---EventListener--->
+
+const button = document.querySelector("#myButton");
+
+button.addEventListener("click", () => {
+  console.log("You clicked on #myID");
+});
+
+        </body>
+        <button class="accordion">Section 1</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+<button class="accordion">Section 2</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+
+<button class="accordion">Section 3</button>
+<div class="panel">
+  <p>Lorem ipsum...</p>
+</div>
+    <div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/M9kgjEsLG6LMbYC9dl" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/M9kgjEsLG6LMbYC9dl">via GIPHY</a></p><script>
+            document.getElementById("demo").innerHTML = "My First JavaScript";
+        </script>
+        </html>
